577b6db05a4f9e1104937c3884a5a0fd8a093d9e
Antal-Teofil <antalteofil.githubclient@gmail.com>
2026-02-28 02:37:45 +0200

    event grid function

---
+++ InvoiceProcessingPipeline/InvoiceProcessingPipeline.Functions/Events/IncomingDocumentEvent.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Messaging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceProcessingPipeline.Functions.Events
+{
+    public sealed class IncomingDocumentEvent(ILogger<IncomingDocumentEvent> logger)
+    {
+        [Function(nameof(IncomingDocumentEvent))]
+        public async Task RunAsync([EventGridTrigger] CloudEvent cloudEvent, [DurableClient] DurableTaskClient durableClient) 
+        {
+            logger.LogInformation(
+            "CloudEvent received. Id={Id}, Type={Type}, Subject={Subject}",
+            cloudEvent.Id,
+            cloudEvent.Type,
+            cloudEvent.Subject);
+        }
+    }
+}
