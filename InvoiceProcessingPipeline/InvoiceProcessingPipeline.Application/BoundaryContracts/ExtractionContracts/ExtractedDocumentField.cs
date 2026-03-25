214b872d6f5804277b698453d870615d29c17c90
Antal-Teofil <antalteofil.githubclient@gmail.com>
2026-03-25 16:42:23 +0200

    extraction model to be refactored

--- InvoiceProcessingPipeline/InvoiceProcessingPipeline.Application/BoundaryContracts/ExtractionContracts/ExtractedDocumentField.cs
+++ InvoiceProcessingPipeline/InvoiceProcessingPipeline.Application/BoundaryContracts/ExtractionContracts/ExtractedDocumentField.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace InvoiceProcessingPipeline.Application.BoundaryContracts.ExtractionContracts
 {
-    public sealed record ExtractedField<TSource> where TSource : class
+    public sealed record ExtractedDocumentField<TSource> where TSource : class
     {
         public required string FieldName { get; set; }
         public required string FieldValue { get; set; }
-        public required TSource Field { get; set; }
+        public required TSource? Field { get; set; }
         public required float ConfidenceScore { get; set; }
     }
 }
