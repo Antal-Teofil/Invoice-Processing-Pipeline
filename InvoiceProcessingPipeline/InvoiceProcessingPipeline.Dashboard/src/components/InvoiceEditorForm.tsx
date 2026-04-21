48c19c8fe9ba3924228dc49578beb3e4cc752404
Antal-Teofil <antalteofil.githubclient@gmail.com>
2026-04-21 23:35:04 +0300

    start implementing invoice form

--- InvoiceProcessingPipeline/InvoiceProcessingPipeline.Dashboard/src/components/InvoiceEditorForm.tsx
+++ InvoiceProcessingPipeline/InvoiceProcessingPipeline.Dashboard/src/components/InvoiceEditorForm.tsx
@@ -1,10 +1,20 @@
+import { useForm } from "@tanstack/react-form"
+import type Invoice from "../types/InvoiceSchema";
 
-export default function InvoiceEditorForm() {
+export default function InvoiceEditorForm( {} : Invoice) {
 
-    
+    const invoiceForm = useForm({
+
+    });
+
     return (
-        <>
-        
-        </>
+        <div>
+            <h1>INVOICE</h1>
+            <form>
+                <invoiceForm.Field name="">
+
+                </invoiceForm.Field>
+            </form>
+        </div>
     );
 }
