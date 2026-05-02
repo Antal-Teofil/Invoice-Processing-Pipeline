3c0a1d73bac282b036b12675dcf33d887d4e887c
Antal-Teofil <antalteofil.githubclient@gmail.com>
2026-05-02 13:15:19 +0300

    qeqfrgt

--- InvoiceProcessingPipeline/InvoiceProcessingPipeline.Domain/CommonDefinitions/CommercialInvoiceDocumentBuilder.cs
+++ InvoiceProcessingPipeline/InvoiceProcessingPipeline.Domain/CommonDefinitions/CommercialInvoiceDocumentBuilder.cs
@@ -1,4 +1,5 @@
-﻿using InvoiceProcessingPipeline.Domain.Aggregates.DocumentTypes;
+﻿using InvoiceProcessingPipeline.Domain.Aggregates.Components;
+using InvoiceProcessingPipeline.Domain.Aggregates.DocumentTypes;
 using InvoiceProcessingPipeline.Domain.ExtractionContracts;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,54 @@ namespace InvoiceProcessingPipeline.Domain.CommonDefinitions
 {
     public sealed class CommercialInvoiceDocumentBuilder(ExtractedDocumentData extraction) : InvoiceDocumentSchemeBuilder<CommercialInvoice>(extraction)
     {
+        public CommercialInvoice Document { get; init; } = new();
         public override CommercialInvoice Build()
         {
-            return new CommercialInvoice();
+            return Document;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddAccountingCustomerParty()
+        {
+            if(Extraction.TryGetField("accountingCustomerParty", out ExtractedDocumentField<Party>? field))
+            {
+
+            }
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddAccountingSupplierParty()
+        {
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddInvoiceNumber()
+        {
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddIssueDate()
+        {
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddDueDate()
+        {
+            return this;
+        }
+        
+        public CommercialInvoiceDocumentBuilder AddDocumentCurrencyCode()
+        {
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddLegalMonetaryTotal()
+        {
+            return this;
+        }
+
+        public CommercialInvoiceDocumentBuilder AddInvoiceLine()
+        {
+            return this;
         }
     }
 }
